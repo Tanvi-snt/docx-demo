ac79821e6e2cab9186c6700e9113ea368413e0c2
Shivsnt40 <s.kale@splendornet.com>
2021-07-15 22:32:10 +0530

    new-changes

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+<div>
+    <add-blog></add-blog>
+    <show-blog></show-blog>
+    <list-blog></list-blog>
+</div>
+  
+</template>
+
+<script>
+import addBlog from "./addBlog.vue";
+import showBlog from "./showBlog.vue";
+import listBlog from "./listBlog.vue"
+
+export default {
+  components: {
+    "add-blog": addBlog,
+    "show-blog":showBlog,
+    "list-blog":listBlog
+  },
+
+  data() {
+    return {
+    }
+  },
+  methods: {
+   
+  }
+};
+</script>
+
+
+
+<style scoped>
+
+</style>
---
+++ src/App1.vue
@@ -0,0 +1,48 @@
+<template>
+<div>
+    <h1>{{ name }}</h1>
+   <ninjas></ninjas>
+</div>
+
+
+</template>
+
+<script>
+import Ninjas from './Ninjas.vue'
+export default {
+  components: { Ninjas },
+
+ 
+  data () {
+   
+    return {
+      name: 'I am tanvi lanjekar'
+    }
+  },
+  methods:{
+    greeting:function(){
+      return 'This is my first vue cli demo!!!!'
+    }
+  }
+}
+</script>
+
+
+
+<style scoped>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: bold;
+  color:green;
+}
+
+
+</style>
