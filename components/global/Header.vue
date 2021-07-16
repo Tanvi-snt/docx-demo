06fc7f5ad53f1f2afcaf5d0910d7cfb8a0d966f3
Shivsnt40 <s.kale@splendornet.com>
2021-07-16 17:22:10 +0530

    new-changes

--- components/global/Header.vue
+++ components/global/Header.vue
@@ -1,5 +1,56 @@
 <template>
-  <nav></nav>
+  <nav>
+    <div>
+      <div>
+        <img src="" alt="" />
+      </div>
+      <div>
+        <ul>
+          <li><a href="">What's different</a></li>
+          <li class="dropdown">
+            <a
+              class="nav-link dropdown-toggle"
+              href="#"
+              id="navbarDropdown"
+              role="button"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              Product
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <li><a class="dropdown-item" href="#">Action</a></li>
+              <li><a class="dropdown-item" href="#">Another action</a></li>
+              <li><hr class="dropdown-divider" /></li>
+              <li><a class="dropdown-item" href="#">Something else here</a></li>
+            </ul>
+          </li>
+          <li class="dropdown">
+            <a
+              class="nav-link dropdown-toggle"
+              href="#"
+              id="navbarDropdown"
+              role="button"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              Resources
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <li><a class="dropdown-item" href="#">Action</a></li>
+              <li><a class="dropdown-item" href="#">Another action</a></li>
+              <li><hr class="dropdown-divider" /></li>
+              <li><a class="dropdown-item" href="#">Something else here</a></li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <button type="submit">Sign in</button>
+        <button type="submit">Get early access</button>
+      </div>
+    </div>
+  </nav>
 </template>
 
 <script>
